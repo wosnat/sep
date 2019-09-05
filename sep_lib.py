95b53bffab3c63069467ff7cc6c9fd09a3067b91
Osnat Weisberg <osnat@sher-ws>
2019-09-05 14:30:13 +0300

    pycharm settings

--- sep_lib.py
+++ sep_lib.py
@@ -1,5 +1,7 @@
 import itertools
 import sys, os
+import logging
+from collections import defaultdict
 
 from scipy.special import comb
 from scipy import stats
@@ -32,201 +34,214 @@ from scipy import stats
 
 import pysam
 
-def _read_calculate_overlap(read,
-                            gene_start, gene_end,
-                            gene_is_reversed):
-    start = max(0, gene_start - 1)
-    stop = gene_end
-    # utr_start / utr_stop
-    utr_start = start
-    utr_stop = stop
-    if gene_is_reversed:
-        utr_stop = utr_stop + 100
-    else:
-        utr_start = max(0, utr_start - 100)
-    overlap = read.get_overlap(start, stop)
-    read_overflow = (read.reference_end - read.reference_start) - overlap
-    utr_overlap = read.get_overlap(utr_start, utr_stop)
-    read_utr_overflow = (read.reference_end - read.reference_start) - utr_overlap
+class RnaAalignedRead:
+    def __init__(self):
+        self.read_start = 0
+        self.read_end = 0
+        self.read_is_reverse = 0
+        self.read_mapping_quality_sum = 0
+        self.read_overflow_sum = 0
+        self.read_utr_overflow_sum = 0
+        self.read_count = 0
 
-    return read_overflow, read_utr_overflow
+    def add_read(self, read :pysam.AlignedSegment , gene_start:int, gene_end:int, gene_is_reversed:bool):
+        self.read_start = read.reference_start + 1
+        self.read_end = read.reference_end
+        self.read_is_reverse = read.is_reverse
+        self.read_mapping_quality_sum += read.mapping_quality
+        read_overflow, read_utr_overflow = \
+            self._read_calculate_overlap(read, gene_start, gene_end, gene_is_reversed)
+        self.read_overflow_sum += read_overflow
+        self.read_utr_overflow_sum += read_utr_overflow
+        self.read_count += 1
 
+    def _read_calculate_overlap(self, read:pysam.AlignedSegment , gene_start:int, gene_end:int,
+                                gene_is_reversed:bool):
+        """
+        calculate the overflow between the read and the gene. utr overflow: allow 100 bases before gene
+        starts for utr (untranslated region)
+        :param read:  pysam.AlignedSegment
+        :param gene_start: int start index beginning of reading frame). 1 based.
+        :param gene_end: int end index (one ater the gene ends). 1 based.
+        :param gene_is_reversed: bool is the gene on the reverse (antisense) strand
+        :return: tuple (int, int): (read overflow, read utr overflow)
+        """
+        start = max(0, gene_start - 1)
+        stop = gene_end
+        # utr_start / utr_stop
+        utr_start = start
+        utr_stop = stop
+        if gene_is_reversed:
+            utr_stop = utr_stop + 100
+        else:
+            utr_start = max(0, utr_start - 100)
+        overlap = read.get_overlap(start, stop)
+        read_overflow = (read.reference_end - read.reference_start) - overlap
+        utr_overlap = read.get_overlap(utr_start, utr_stop)
+        read_utr_overflow = (read.reference_end - read.reference_start) - utr_overlap
 
-def _read_add_to_dict(read_dict, read,
-                      gene_start, gene_end,
-                      gene_is_reversed):
-    read_start = read.reference_start + 1
-    read_end = read.reference_end
-    read_is_reverse = read.is_reverse
-    read_mapping_quality = read.mapping_quality
-    read_overflow, read_utr_overflow = _read_calculate_overlap(read, gene_start, gene_end, gene_is_reversed)
+        return read_overflow, read_utr_overflow
 
-    key = (read_start, read_end, read_is_reverse)
-    read_dict.setdefault(key, [0, 0, 0, 0])
-    read_dict[key][0] += read_mapping_quality
-    read_dict[key][1] += 1
-    read_dict[key][2] += read_overflow
-    read_dict[key][3] += read_utr_overflow
+    @property
+    def read_mapping_quality(self):
+        return self.read_mapping_quality_sum / self.read_count
 
+    @property
+    def read_overflow(self):
+        return self.read_overflow_sum / self.read_count
 
-def _read_decode_key_value(key, value):
-    (read_start, read_end, read_is_reverse) = key
-    (read_mapping_quality_sum, read_count,
-     read_overflow_sum, read_utr_overflow_sum) = value
+    @property
+    def read_utr_overflow(self):
+        return self.read_utr_overflow_sum / self.read_count
 
-    read_mapping_quality = read_mapping_quality_sum / read_count
-    read_overflow = read_overflow_sum / read_count
-    read_utr_overflow = read_utr_overflow_sum / read_count
-    return (read_start, read_end, read_is_reverse, read_count,
-            read_mapping_quality, read_overflow, read_utr_overflow)
+    @staticmethod
+    def read_to_key(read: pysam.AlignedSegment):
+        read_start = read.reference_start + 1
+        read_end = read.reference_end
+        read_is_reverse = read.is_reverse
+        return (read_start, read_end, read_is_reverse)
 
-
-def _read_dict_item_to_record(key, value):
-    (read_start, read_end, read_is_reverse, read_count,
-     read_mapping_quality, read_overflow, read_utr_overflow
-     ) = _read_decode_key_value(key, value)
-
-    return {
-        'read_start': read_start,
-        'read_end': read_end,
-        'read_is_reverse': read_is_reverse,
-        'read_mapping_quality': read_mapping_quality,
-        'read_count': read_count,
-        'read_overflow': read_overflow,
-        'read_utr_overflow': read_utr_overflow,
-    }
-
-
-def _read_dict_item_to_count(key, value, gene_is_reversed, count_overflow):
-    """ return tuple:
-    number_of_reads, number_of_antisense_reads
-
-    """
-    (read_start, read_end, read_is_reverse, read_count,
-     read_mapping_quality, read_overflow, read_utr_overflow
-     ) = _read_decode_key_value(key, value)
-
-    if not count_overflow and read_overflow:
-        return 0, 0
-    if read_is_reverse == gene_is_reversed:
-        return read_count, 0
-    else:
-        return 0, read_count
+    def to_record(self):
+        return {
+            'read_start': self.read_start,
+            'read_end': self.read_end,
+            'read_is_reverse': self.read_is_reverse,
+            'read_mapping_quality': self.read_mapping_quality,
+            'read_count': self.read_count,
+            'read_overflow': self.read_overflow,
+            'read_utr_overflow': self.read_utr_overflow,
+        }
 
 
-# TODO: UTR - 100 bases before gene start
-# TODO: overlap for partial overlap genes
+    def to_count(self, gene_is_reversed :bool, count_overflow: bool):
+        """ return tuple:
+        number_of_reads, number_of_antisense_reads
 
-def _reads_to_dict(samfile, contig,
-                   gene_start, gene_end,
-                   gene_is_reversed):
-    # pysam indexing is 0 based, annotation is 1 based
-    start = max(0, gene_start - 1)
-    stop = gene_end
+        """
+        if not count_overflow and self.read_overflow:
+            return 0, 0
+        if self.read_is_reverse == gene_is_reversed:
+            return self.read_count, 0
+        else:
+            return 0, self.read_count
 
-    if (stop is None) or (np.isnan(stop)):
-        # last gene in the list
-        return dict()
-
-    if stop <= start:
-        return dict()
-    read_dict = dict()
-    for read in samfile.fetch(contig=contig, start=start, stop=stop):
-        # group reads by the start index
-        print(read)
-        _read_add_to_dict(read_dict, read, gene_start, gene_end, gene_is_reversed)
-    return read_dict
-
-
-def reads_per_gene(samfile, contig,
-                   gene_start, gene_end,
-                   gene_is_reversed):
-    read_dict = _reads_to_dict(samfile, contig,
-                               gene_start, gene_end,
-                               gene_is_reversed)
-
-    reads_list = [_read_dict_item_to_record(k, v) for k, v in read_dict.items()]
-
-    return reads_list
+    def to_cover(self, cover_df: pd.DataFrame,
+                gene_start:int, gene_end:int, gene_is_reversed:bool):
+        if (gene_is_reversed == self.read_is_reverse):
+            column = 'reads'
+        else:
+            column = 'reads_as'
+        if self.read_overflow:
+            column = f'overflow_{column}'
+        start = max(self.read_start, gene_start)
+        end = min(self.read_end, gene_end)
+        cover_df.loc[range(start, end), column] = cover_df.loc[range(start, end), column] + self.read_count
 
 
-def count_reads(samfile, contig,
-                gene_start, gene_end,
-                gene_is_reversed, count_overflow):
-    read_dict = _reads_to_dict(samfile, contig,
-                               gene_start, gene_end,
-                               gene_is_reversed)
+class RnaReads:
+    def __init__(self, samfile: pysam.AlignmentFile, contig: str,
+                 gene_start:int, gene_end:int, gene_is_reversed:bool,
+                 gene_type:str):
+        self.reads_dict = defaultdict(RnaAalignedRead)
+        self.gene_start = gene_start
+        self.gene_end = gene_end
+        self.gene_type = gene_type
+        self.gene_is_reversed = gene_is_reversed
 
-    count_list = [_read_dict_item_to_count(k, v, gene_is_reversed, count_overflow)
-                  for k, v in read_dict.items()]
-    if count_list == []:
-        counts = [0, 0]
-    else:
-        counts = list(map(sum, zip(*count_list)))
+        # pysam indexing is 0 based, annotation is 1 based
+        samstart = max(0, gene_start - 1)
+        samstop = gene_end
+
+        if (samstop is None) or (np.isnan(samstop)):
+            # last gene in the list
+            samstop = samfile.get_reference_length()
+        if samstop <= samstart:
+            return
+        for read in samfile.fetch(contig=contig, start=samstart, stop=samstop):
+            # group reads by the start index
+            self.add_read(read)
+
+
+    def add_read(self,read: pysam.AlignedSegment):
+        logging.debug(read)
+        key = RnaAalignedRead.read_to_key(read)
+        self.reads_dict[key].add_read(read, self.gene_start, self.gene_end, self.gene_is_reversed)
+
+
+    def get_record_list(self):
+        return [r.to_record() for r in self.reads_dict.values()]
+
+    def get_counts(self, count_overflow: bool):
+        if not self.reads_dict:
+            return  [0, 0]
+        else:
+            count_list = (r.to_count(self.gene_is_reversed, count_overflow) for r in self.reads_dict.values())
+            counts = list(map(sum, zip(*count_list)))
+            return counts
+
+    def _cover_select_gene_column(self, is_intergenic:bool):
+        if is_intergenic:
+            gene_column = 'gene_inter'
+        if not self.gene_is_reversed:
+            gene_column = f'{self.gene_type}_sense'
+        else:
+            gene_column = f'{self.gene_type}_as'
+        return gene_column
+
+    def _cover_init_df(self, is_intergenic:bool):
+        cover_df_columns = [
+            'gene_inter',
+            'peg_sense', 'peg_as',
+            'rna_sense', 'rna_as',
+            'reads', 'reads_as',
+            'overflow_reads', 'overflow_reads_as',
+        ]
+        cover_df = pd.DataFrame(index=range(self.gene_start, self.gene_end),
+                                columns=cover_df_columns)
+
+        cover_df.loc[:, 'reads'] = 0
+        cover_df.loc[:, 'reads_as'] = 0
+        cover_df.loc[:, 'overflow_reads'] = 0
+        cover_df.loc[:, 'overflow_reads_as'] = 0
+        # mark the gene location
+        gene_column = self._cover_select_gene_column(is_intergenic)
+        cover_df.loc[:, gene_column] = 1
+        return cover_df
+
+    def get_cover_reads(self, is_intergenic:bool):
+        cover_df = self._cover_init_df(is_intergenic)
+        for r in self.reads_dict.values():
+            r.to_cover(cover_df, self.gene_start, self.gene_end, self.gene_is_reversed)
+        return cover_df
+
+
+############################################################3
+
+def reads_per_gene(samfile: pysam.AlignmentFile, contig: str,
+                 gene_start:int, gene_end:int, gene_is_reversed:bool,
+                 gene_type:str):
+    read_dict = RnaReads(samfile, contig, gene_start, gene_end, gene_is_reversed, gene_type)
+    return read_dict.get_record_list()
+
+
+def count_reads(samfile: pysam.AlignmentFile, contig: str,
+                 gene_start:int, gene_end:int, gene_is_reversed:bool,
+                 gene_type:str, count_overflow):
+    read_dict = RnaReads(samfile, contig, gene_start, gene_end, gene_is_reversed, gene_type)
+
+    counts = read_dict.get_counts(count_overflow)
     return pd.Series({'reads': counts[0], 'as_reads': counts[1]})
 
-
-def _read_dict_item_to_cover(cover_df,
-                             gene_start, gene_end, gene_is_reversed,
-                             key, value):
-    (read_start, read_end, read_is_reverse, read_count,
-     read_mapping_quality, read_overflow, read_utr_overflow
-     ) = _read_decode_key_value(key, value)
-    if (gene_is_reversed == read_is_reverse):
-        column = 'reads'
-    else:
-        column = 'reads_as'
-    if read_overflow:
-        column = f'overflow_{column}'
-    start = max(read_start, gene_start)
-    end = min(read_end, gene_end)
-    cover_df.loc[range(start, end), column] = cover_df.loc[range(start, end), column] + read_count
-
-
-def _select_gene_column(gene_type, gene_is_reversed, is_intergenic):
-    if not gene_is_reversed:
-        gene_column = f'{gene_type}_sense'
-    else:
-        gene_column = f'{gene_type}_as'
-    if is_intergenic:
-        gene_column = 'gene_inter'
-    return gene_column
-
-
-def cover_reads(samfile, contig,
-                gene_start, gene_end,
-                gene_is_reversed, gene_type, is_intergenic):
-    read_dict = _reads_to_dict(samfile, contig,
-                               gene_start, gene_end,
-                               gene_is_reversed)
-
-    cover_df_columns = [
-        'gene_inter',
-        'peg_sense', 'peg_as',
-        'rna_sense', 'rna_as',
-        'reads', 'reads_as',
-        'overflow_reads', 'overflow_reads_as',
-    ]
-    cover_df = pd.DataFrame(index=range(gene_start, gene_end),
-                            columns=cover_df_columns)
-
-    cover_df.loc[:, 'reads'] = 0
-    cover_df.loc[:, 'reads_as'] = 0
-    cover_df.loc[:, 'overflow_reads'] = 0
-    cover_df.loc[:, 'overflow_reads_as'] = 0
-    # mark the gene location
-    gene_column = _select_gene_column(gene_type, gene_is_reversed, is_intergenic)
-    cover_df.loc[:, gene_column] = 1
-
-    for k, v in read_dict.items():
-        _read_dict_item_to_cover(cover_df,
-                                 gene_start, gene_end, gene_is_reversed,
-                                 k, v)
-
+def cover_reads(samfile: pysam.AlignmentFile, contig: str,
+                 gene_start:int, gene_end:int, gene_is_reversed:bool,
+                 gene_type:str, is_intergenic:bool):
+    read_dict = RnaReads(samfile, contig, gene_start, gene_end, gene_is_reversed, gene_type)
+    cover_df = read_dict.get_cover_reads(is_intergenic)
     return cover_df
 
 
-def add_useful_columns_to_annotation_df(ann_df):
+
+def add_useful_columns_to_annotation_df(ann_df:pd.DataFrame):
     ann_df['min_idx'] = ann_df[['start', 'stop']].min(axis=1)
     ann_df['max_idx'] = ann_df[['start', 'stop']].max(axis=1)
     ann_df['inter_stop_idx'] = ann_df.min_idx.shift(-1)
@@ -236,35 +251,50 @@ def add_useful_columns_to_annotation_df(ann_df):
     ann_df['inter_length'] = ann_df.inter_length.fillna(0)
     return ann_df
 
-def add_read_counts_to_annotation_df(ann_df, samfile, contig):
-
+def add_read_counts_to_annotation_df(ann_df:pd.DataFrame, samfile: pysam.AlignmentFile, contig:str):
     count_func = lambda x: count_reads(samfile, contig,
-            x['min_idx'], x['max_idx'], x['gene_is_reversed'], count_overflow=True)
-    count_df = ann_df.apply(count_func, axis=1)
+            x['min_idx'], x['max_idx'], x['gene_is_reversed'], x['type'], count_overflow=True)
     inter_count_func = lambda x: count_reads(samfile, contig,
              x['max_idx'], x['inter_stop_idx'],
-             gene_is_reversed=False, count_overflow=False)
+             gene_is_reversed=False, gene_type='intergenic', count_overflow=False)
+
+    count_df = ann_df.apply(count_func, axis=1)
     inter_count_df = ann_df.apply(inter_count_func, axis=1)
     df = ann_df.join(count_df)
     df = df.join(inter_count_df, rsuffix='_inter')
     return df
 
+def create_cover_df(ann_df:pd.DataFrame, samfile: pysam.AlignmentFile, contig:str):
+    cover_func = lambda x: cover_reads(samfile, contig,
+                       x.iloc[0]['min_idx'], x.iloc[0]['max_idx'],
+                       x.iloc[0]['gene_is_reversed'],
+                       x.iloc[0]['type'], is_intergenic=False)
+    group_cols = ['contig_id', 'gene_id', 'type']
+    cover_df = ann_df.groupby(group_cols).apply(cover_func)
+    cover_df.reset_index(inplace=True)
+    cover_df.rename(columns={'level_3': 'location'}, inplace=True)
+    for c in ['gene_inter', 'peg_sense', 'peg_as', 'rna_sense', 'rna_as', 'reads', 'reads_as']:
+        cover_df.loc[:,c] = pd.to_numeric(cover_df[c])
+
+
+    return cover_df
 
 
 if __name__ == '__main__':
     import pprint
 
     pd.set_option('display.max_columns', 500)
-    ann_df = pd.read_csv('MIT9313.txt', sep='\t')
+    ann_df = pd.read_csv('data/MIT9313.txt', sep='\t')
     ann_df = add_useful_columns_to_annotation_df(ann_df)
-    samfile = pysam.AlignmentFile("SRR3334788.sorted.bam", "rb")
-    #lst = reads_per_gene(samfile, 'BX548175.1', 15, 20, False)
-    #print(len(lst))
-    #print(sum(r['read_count'] for r in lst))
-    #pprint.pprint(lst)
+    #samfile = pysam.AlignmentFile("data/SRR3334788.sorted.bam", "rb")
+    samfile = pysam.AlignmentFile("data/mit9313_SRR3334787.sorted.bam", "rb")
+    lst = reads_per_gene(samfile, 'BX548175.1', 15, 20, False,'peg')
+    print(len(lst))
+    print(sum(r['read_count'] for r in lst))
+    pprint.pprint(lst)
 
     #print(count_reads(samfile, 'BX548175.1', 15, 20, gene_is_reversed=True, count_overflow=True))
-    #print(count_reads(samfile, 'BX548175.1', 15, 20, gene_is_reversed=False, count_overflow=True))
+    print(count_reads(samfile, 'BX548175.1', 15, 20, gene_is_reversed=False, gene_type='peg' ,count_overflow=True))
     #print(count_reads(samfile, 'BX548175.1', 15, 20, gene_is_reversed=False, count_overflow=False))
 
     cover_df = cover_reads(samfile, 'BX548175.1', 15, 25, False, 'peg', False)
